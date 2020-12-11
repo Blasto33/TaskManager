bc531f1b96a66813af0ef6b69f5d09455d971a1f
Thomas de Barros <thomas.de-barros@epitech.eu>
2020-12-12 00:32:34 +0100

    the bot is now able to join the user's room and play a (static) music

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
+const { GuildChannel, GuildChannelManager } = require('discord.js');
 const Commando = require('discord.js-commando');
 const bot = new Commando.CommandoClient();
 const TOKEN = process.env.BOT_TOKEN;
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const streamOptions = { seek: 0, volume: 1};
 var prefix = '/';
 
 /* On enregistre les différents dossiers */
@@ -23,7 +27,37 @@ bot.on('message', message => {
         console.log("[SYSTEM] " + message.author.username + " asked the bot for help!");
     }
     if (message.content === "running 90s") {
+        // Not working at this point 
         message.reply("!join https://youtu.be/XCiDuy4mrWU?t=85");
+    }
+
+    if (message.content.toLowerCase().startsWith("!play")) {
+        var channelToJoin = message.member.voice.channel;
+
+        if (channelToJoin != null) {
+            //console.log(channelID);
+            channelToJoin.join()
+            .then(connection => {
+                console.log("Task Manager joined the channel " + channelToJoin.name);
+                const stream = ytdl('https://www.youtube.com/watch?v=9YHTVML4PTE', { filter : 'audioonly' });
+                const dispatcher = connection.play(stream, streamOptions);
+            })
+            .catch();
+            message.reply("Task Manager has joined the voice channel and is ready to go!")
+        }
+        else {
+            message.reply("You must be in a voice channel to play music.")
+        }
+    }
+    
+    if (message.content.toLowerCase().startsWith("!leave")) {
+        var channelToLeave = message.member.voice.channel;
+ 
+        if (channelToLeave != null) {
+            channelToLeave.leave();
+        } else {
+            message.reply("The bot is currently not in a voice channel.")
+        }
     }
 });
 
